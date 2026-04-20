6da712acdf17b348241dfd9efc308d16337bf7d4
Andreyap27 <yap.andre27@gmail.com>
2026-04-20 12:25:53 +0700

    add enum pinjaman to table

---
+++ database/migrations/2026_04_20_000001_add_pinjaman_to_expenses_category_enum.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        \DB::statement("ALTER TABLE expenses MODIFY COLUMN category ENUM('Gaji','Spare Part','Jasa','Maintenance','Pinjaman','BBM ME','BBM AE','Umum','Fee','Lori') NOT NULL COMMENT 'Kategori pengeluaran'");
+    }
+
+    public function down(): void
+    {
+        \DB::statement("ALTER TABLE expenses MODIFY COLUMN category ENUM('Gaji','Spare Part','Jasa','Maintenance','BBM ME','BBM AE','Umum','Fee','Lori') NOT NULL COMMENT 'Kategori pengeluaran'");
+    }
+};
